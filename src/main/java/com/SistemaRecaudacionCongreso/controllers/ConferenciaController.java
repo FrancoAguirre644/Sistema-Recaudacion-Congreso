4fc9a038261e9c0f50641d614a3668a7fc5dc454
Franco <FrancoAguirre644@gmail.com>
2020-06-18 00:42:56 -0300

    Funcionalidad de actualizar agregada

--- src/main/java/com/SistemaRecaudacionCongreso/controllers/ConferenciaController.java
+++ src/main/java/com/SistemaRecaudacionCongreso/controllers/ConferenciaController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.SistemaRecaudacionCongreso.helpers.ViewRouteHelpers;
+import com.SistemaRecaudacionCongreso.models.ConferenciaModel;
 import com.SistemaRecaudacionCongreso.services.IConferenciaService;
 
 @Controller
@@ -30,6 +34,18 @@ public class ConferenciaController {
 	@GetMapping("/delete/{id}")
 	public RedirectView delete(@PathVariable("id") long idConferencia) {
 		conferenciaService.remove(idConferencia);
+		return new RedirectView(ViewRouteHelpers.CONFERENCIA_ROOT);
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseBody
+	public ConferenciaModel get(@PathVariable("id") long idConferencia) {
+		return conferenciaService.findByIdConferencia(idConferencia);
+	}
+	
+	@PostMapping("/save")
+	public RedirectView save(@ModelAttribute("conferencia") ConferenciaModel conferenciaModel) {
+		conferenciaService.insertOrUpdate(conferenciaModel);
 		return new RedirectView(ViewRouteHelpers.CONFERENCIA_ROOT);
 	}
 
--- src/main/java/com/SistemaRecaudacionCongreso/services/implementation/ConferenciaService.java
+++ src/main/java/com/SistemaRecaudacionCongreso/services/implementation/ConferenciaService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.SistemaRecaudacionCongreso.converters.ConferenciaConverter;
 import com.SistemaRecaudacionCongreso.entities.Conferencia;
+import com.SistemaRecaudacionCongreso.models.ConferenciaModel;
 import com.SistemaRecaudacionCongreso.repositories.IConferenciaRepository;
 import com.SistemaRecaudacionCongreso.services.IConferenciaService;
 
@@ -16,6 +18,10 @@ public class ConferenciaService implements IConferenciaService{
 	@Autowired
 	@Qualifier("conferenciaRepository")
 	private IConferenciaRepository conferenciaRepository;
+	
+	@Autowired
+	@Qualifier("conferenciaConverter")
+	private ConferenciaConverter conferenciaConverter;
 
 	@Override
 	public List<Conferencia> getAll() {
@@ -26,6 +32,18 @@ public class ConferenciaService implements IConferenciaService{
 	public void remove(long idConferencia) {
 		conferenciaRepository.deleteById(idConferencia);
 	}
+	
+	public ConferenciaModel findByIdConferencia(long idConferencia) {
+		return conferenciaConverter.entityToModel(conferenciaRepository.findByIdConferencia(idConferencia));
+	}
+
+	@Override
+	public ConferenciaModel insertOrUpdate(ConferenciaModel conferenciaModel) {
+		// TODO Auto-generated method stub
+		Conferencia conferencia = conferenciaRepository.save(conferenciaConverter.modelToEntity(conferenciaModel));
+		return conferenciaConverter.entityToModel(conferencia);
+	}
+
 	
 
 }
