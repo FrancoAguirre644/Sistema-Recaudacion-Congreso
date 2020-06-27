b05f2ccd0ea8dfb9501887d21b94664a7ef94baf
Franco <FrancoAguirre644@gmail.com>
2020-06-27 17:01:15 -0300

    Caso de uso cantidad de espectadores del congreso por nivel de estudio

--- src/main/java/com/SistemaRecaudacionCongreso/controllers/HomeController.java
+++ src/main/java/com/SistemaRecaudacionCongreso/controllers/HomeController.java
@@ -1,15 +1,13 @@
 package com.SistemaRecaudacionCongreso.controllers;
 
-import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.SistemaRecaudacionCongreso.models.RankingConferenciaModel;
+import com.SistemaRecaudacionCongreso.helpers.ViewRouteHelpers;
 import com.SistemaRecaudacionCongreso.services.IConferenciaService;
 
 @Controller
@@ -21,12 +19,9 @@ public class HomeController {
 	private IConferenciaService conferenciaService;
 	
 	@GetMapping("")
-	public ModelAndView home() {
-		ModelAndView mAV = new ModelAndView("/home/index");
-		
-		mAV.addObject("ranking", conferenciaService.getRanking());
-		
-		return mAV;
+	public String home() {
+				
+		return ViewRouteHelpers.HOME_INDEX;
 	}
 	
 
--- src/main/java/com/SistemaRecaudacionCongreso/services/IEntradaService.java
+++ src/main/java/com/SistemaRecaudacionCongreso/services/IEntradaService.java
@@ -1,9 +1,11 @@
 package com.SistemaRecaudacionCongreso.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.SistemaRecaudacionCongreso.entities.Entrada;
 import com.SistemaRecaudacionCongreso.models.EntradaModel;
+import com.SistemaRecaudacionCongreso.models.RankingConferenciaModel;
 
 public interface IEntradaService {
 	
@@ -14,5 +16,7 @@ public interface IEntradaService {
 	public EntradaModel findByIdEntrada(long idEntrada);
 
 	public EntradaModel insertOrUpdate(EntradaModel entradaModel);
+	
+	public ArrayList<RankingConferenciaModel> getCantidadEspectadoresNivelEstudio();  
 
 }
